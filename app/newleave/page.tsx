4a2b07a9b9c529abb2b131985a88de56ab12462d
Yug Shara <21sharmayug@gmail.com>
2025-09-11 13:44:41 +0530

    Leave Request done

---
+++ app/newleave/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+import Desktop13 from "@/src/pages/Desktop13";
+
+export default function HomePage() {
+  return <Desktop13 />;
+}
+
+
--- src/pages/Desktop13/index.jsx
+++ src/pages/Desktop13/index.jsx
@@ -1,16 +1,25 @@
 "use client";
 import React from "react";
-import New_leave from "./Newleave"
 import Sidebar from "../../components/ui/Sidebar";
 import Header from "./Header";
-
+import Newleave from "./Newleave";
 
 export default function IndexPage() {
   return (
     <div className="flex h-screen bg-gray-50">
-      
-       <New_leave />
-      
+      {/* Sidebar on the left */}
+      <Sidebar />
+
+      {/* Main content on the right */}
+      <div className="flex flex-col flex-1">
+        {/* Header at the top */}
+        <Header />
+
+        {/* Page Content (New Leave Section) */}
+        <main className="flex-1 p-4 md:p-6 lg:p-8 overflow-y-auto">
+          <Newleave />
+        </main>
+      </div>
     </div>
   );
 }
