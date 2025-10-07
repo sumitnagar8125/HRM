cb51bc321f03bb34be7ded32b6b0fb687fa3dccd
Yug Shara <21sharmayug@gmail.com>
2025-10-07 14:45:57 +0530

    Nothing much changed

--- src/pages/Desktop13/Newleave.jsx
+++ src/pages/Desktop13/Newleave.jsx
@@ -42,9 +42,13 @@ export default function Newleave() {
             </label>
             <select className="mt-2 w-full border rounded-lg px-3 py-2 text-sm focus:ring-2 focus:ring-purple-500 outline-none">
               <option value="">Select a Policy</option>
-              <option value="Sick">Vacation</option>
-              <option value="Casual">Medical</option>
-              <option value="Earned">Personal</option>
+              <option value="Sick">Vacation Leave</option>
+              <option value="Casual">Medical Leave</option>
+              <option value="Earned">Personal Leave</option>
+              <option value="Sick">Sick Leave</option>
+              <option value="Casual">Emergency Leave</option>
+              <option value="Earned">Maternity Leave</option>
+              <option value="Earned">Paternity Leave</option>
             </select>
           </div>
 
